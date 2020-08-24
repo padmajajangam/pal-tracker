170c41627db724dcae341476f8d722ca11bf1cbd
padmajajangam <sweetpaddy@gmail.com>
2020-08-24 11:45:21 +0000

    manifest changes `:wq

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace PalTracker
 {
+    
     [Route("/")]
     public class WelcomeController : ControllerBase
     {
+        WelcomeMessage _message;
+        public WelcomeController(WelcomeMessage welcomeMessage)
+        {
+            _message = welcomeMessage;
+            
+        }
+
         [HttpGet]
-        public string SayHello() => "hello";
+        public string SayHello() => _message.Message;
     }
 }
